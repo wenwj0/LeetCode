7f50edd8bcfee1677fe2d3d11effc588b94bcf8c
wenwj0 <over.7456@qq.com>
2018-11-25 22:27:35 +0800

    update

---
+++ Array/053最大子序和/P53.java
@@ -0,0 +1,22 @@
+package leetcodeTest;
+
+public class P53 {
+	public int maxSubArray(int[] nums) {
+		int len = nums.length;
+		int maxn = nums[0];
+		int temp = nums[0];
+		for (int i=1;i<len;i++) {
+			if(nums[i] + temp<nums[i]) {
+				temp = nums[i];
+			}else {
+				temp = nums[i] + temp;
+			}
+			if(temp > maxn) {
+				maxn = temp;
+			}
+		}
+		
+		
+		return maxn; 
+	}
+}
