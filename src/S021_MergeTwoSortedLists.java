8b6479d11143aa4e08db7de8b1c2ea9fd04c4dc9
Jan <over.7456@qq.com>
2020-08-14 01:13:51 +0800

    update

---
+++ src/S021_MergeTwoSortedLists.java
@@ -0,0 +1,18 @@
+public class S021_MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode p = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val > l2.val) {
+                p.next = l2;
+                l2 = l2.next;
+            } else {
+                p.next = l1;
+                l1 = l1.next;
+            }
+            p = p.next;
+        }
+        p.next = l1 == null ? l2 : l1;
+        return dummy.next;
+    }
+}
