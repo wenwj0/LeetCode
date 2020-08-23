ba7f8e32a3161288a180cd63e206fac83d02d2da
Jan <over.7456@qq.com>
2020-08-24 00:22:44 +0800

    update

---
+++ src/S026_RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,23 @@
+public class S026_RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        if(nums==null || nums.length==0)
+            return 0;
+        if(nums.length==1)
+            return 1;
+        int left = 0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[left]){
+                continue;
+            }else {
+                left++;
+                nums[left] = nums[i];
+            }
+        }
+        return left+1;
+    }
+
+    public static void main(String[] args) {
+        int len= new S026_RemoveDuplicatesFromSortedArray().removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4});
+        System.out.println(len);
+    }
+}
---
+++ src/S028_ImplementStrStr.java
@@ -0,0 +1,38 @@
+public class S028_ImplementStrStr {
+    public int strStr(String haystack, String needle) {
+        if (needle == null || needle.length() == 0)
+            return 0;
+        for (int i = 0; i + needle.length() <= haystack.length(); i++) {
+            if (haystack.substring(i, i + needle.length()).equals(needle))
+                return i;
+        }
+        return -1;
+    }
+
+    public int strStr1(String haystack, String needle) {
+        if (needle == null || needle.length() == 0)
+            return 0;
+        char[] hs = haystack.toCharArray();
+        char[] nd = needle.toCharArray();
+        for (int i = 0; i < hs.length; i++) {
+            if (hs[i] != nd[0]) {
+                continue;
+            } else {
+                int k = 0;
+                for (int j = 0; j < needle.length(); j++, k++) {
+                    if (k + i >= haystack.length() || hs[i + k] != nd[j])
+                        break;
+                }
+                if (k == nd.length)
+                    return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String hs = "";
+        String nd = "0";
+        System.out.println(new S028_ImplementStrStr().strStr(hs, nd));
+    }
+}
